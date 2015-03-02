6d9710496990f5c79234369737110d060246d84a
Brian Soe <bsoe@ucsd.edu>
2015-03-02 03:04:45 -0800

    fixed dynamic server control

--- server.py
+++ server.py
@@ -7,6 +7,5 @@ Description: Activates server for Flask application.
 from dev import app, routes
 import os
 
-#app.run(debug=True) # debug on for constant update upon change
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)                                      
+app.run(host='0.0.0.0', port=port, debug=True)                                      
