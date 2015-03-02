677b004356af33897c25a6572e006aa7a79cd532
Brian Soe <bsoe@ucsd.edu>
2015-03-02 02:59:40 -0800

    port handling continued

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Description: Activates server for Flask application.
 """
 
 from dev import app, routes
+import os
+
 #app.run(debug=True) # debug on for constant update upon change
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)                                      
