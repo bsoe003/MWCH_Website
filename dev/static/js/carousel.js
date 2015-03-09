3ad34738d17103c8ca474cc59677bb3e3cdd1225
Christine Do <christinevdo@gmail.com>
2015-03-09 09:31:38 -0700

    added carousel with placeholder images- still not perfect

---
+++ dev/static/js/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.single-item').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
